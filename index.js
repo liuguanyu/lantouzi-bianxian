2b62fe2e820e7aef15e39584a3aa78e5b26fd4ab
liuguanyu <lgy573@126.com>
2015-12-23 11:35:27 +0800

    -

---
+++ index.js
@@ -0,0 +1,49 @@
+var request = require("request");
+var cheerio = require("cheerio");
+
+var urlTmp = "https://lantouzi.com/bianxianjihua/index?page=%d&size=14&tag=2";
+var arr = [1,2,3,4,5,6];
+var REQUEST_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:13.0) Gecko/20100101 Firefox/13.0',
+};
+
+var minProfit = 8.00;
+var maxDays = 100;
+
+var pros = arr.map(function (el){
+    var url = urlTmp.replace(/%d/, el);
+
+    return new Promise(function (resolve, reject){
+        request({
+            url : url,
+            header: REQUEST_HEADERS
+        },  function(err, httpResponse, body) {
+            if (err){
+                reject(err);
+            } 
+
+            resolve(body);
+        });
+    }); 
+});
+
+Promise.all(pros).then(function(data){
+    data = data.reduce(function (prev, current){
+        var $ = cheerio.load(current, { decodeEntities: false });
+
+        var lists = $(".project-list li").filter(function (){
+            var profit = $(this).find(".info-one em").html().replace('%', '');
+            var days = $(this).find(".info-two em").html();
+
+            return parseFloat(profit) >= minProfit && parseInt(days, 10) <= maxDays 
+        });
+
+        lists = lists.map(function (){
+            return [$(this).find(".info-one em").html(),  $(this).find(".info-two em").html() + "天",  $(this).find(".info-four a").attr("href")].join('\t');  
+        }).toArray();
+
+        return prev.concat(lists);
+    }, []);
+
+    console.log(data.join("\n"));
+});
